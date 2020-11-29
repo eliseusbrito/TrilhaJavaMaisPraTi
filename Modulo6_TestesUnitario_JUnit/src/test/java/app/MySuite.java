3e3bb86d0f7c7721d57d94756228b3da790739e1
eliseusbrito <eliseusbrito@gmail.com>
2020-11-28 22:24:49 -0300

    Criado Suite de Testes

---
+++ Modulo6_TestesUnitario_JUnit/src/test/java/app/MySuite.java
@@ -0,0 +1,9 @@
+package app;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayUtilTest.class, StringUtilTest.class})
+public class MySuite {
+}
